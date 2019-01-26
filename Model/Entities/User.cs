7656a6952d6f3cdc81eb13b8605c51194cb89260
Franco Arto <francoarto@gmail.com>
2019-01-26 13:23:27 -0300

    Model folders and user entity

---
+++ Model/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    public class User
+    {
+        int UserId { get; set; }
+        string Email { get; set; }
+        string UserPassword { get; set; }
+        DateTime LastLogin { get; set; }
+        int TypeId { get; set;}
+
+
+    }
+}
