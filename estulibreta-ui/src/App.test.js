c8b44b0cebc3a4196500f6246ca74d50528656dd
Franco Arto <francoarto@gmail.com>
2019-10-19 15:13:00 -0300

    Add redux and enzyme. Test enzyme with hooks

--- estulibreta-ui/src/App.test.js
+++ estulibreta-ui/src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { shallow } from 'enzyme';
 import App from './App';
 
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('.App').text()).toBe("1");
 });
